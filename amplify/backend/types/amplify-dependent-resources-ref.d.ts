50769fce91ea8cbb61e29e65d99523740ed3dd97
Johann Guepjop <guepjopjohan@gmail.com>
2023-05-21 14:54:25 -0700

    adding basic graphql api to react app

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,9 @@
+export type AmplifyDependentResourcesAttributes = {
+  "api": {
+    "propertymgmt": {
+      "GraphQLAPIEndpointOutput": "string",
+      "GraphQLAPIIdOutput": "string",
+      "GraphQLAPIKeyOutput": "string"
+    }
+  }
+}
