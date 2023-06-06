28112a689df7d76ae092be04f29bde19c1a52fc5
Johann Guepjop <guepjopjohan@gmail.com>
2023-06-05 20:58:07 -0700

    updating schema

--- src/pages/CreateOrganizationPage/hooks/useCreateOrganizationPage.tsx
+++ src/pages/CreateOrganizationPage/hooks/useCreateOrganizationPage.tsx
@@ -2,9 +2,8 @@ import { FormInstance } from "antd";
 import React from "react";
 import { Auth, API } from "aws-amplify";
 import { NewOrganizationForm } from "../../../types";
-import { createOrganization } from "../../../graphql/mutations";
+import { createOrganization, updateUser } from "../../../graphql/mutations";
 import { getUser } from "../../../graphql/queries";
-import { GraphQLList, GraphQLNonNull } from "graphql";
 
 const useCreateOrganizationPage = ({
   formInstance,
@@ -36,17 +35,33 @@ const useCreateOrganizationPage = ({
       console.log("user", user);
 
       const ownerList = [user];
-      const val = await API.graphql({
+      const organization = await API.graphql({
         query: createOrganization,
         variables: {
           input: {
             name: formData.name,
-            //@ts-ignore
-            owners: {},
           },
         },
       });
-      console.log("organization created", val);
+
+      const userUpdate = await API.graphql({
+        query: updateUser,
+        variables: {
+          input: {
+            id: userid,
+            organization: {
+              //@ts-ignore
+              id: organization.data.createOrganization.id,
+              //@ts-ignore
+              name: organization.data.createOrganization.name,
+            },
+            // //@ts-ignore
+            // ...user.data.getUser,
+          },
+        },
+      });
+      console.log("organization created", organization);
+      console.log("updated user", userUpdate);
     } catch (error) {
       console.log("error using api", error);
     }
