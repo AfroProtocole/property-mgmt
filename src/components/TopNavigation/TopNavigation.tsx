d3c65d8f28783a0d5960219b88e3781b5c6fc09a
kelvis24 <elviskimara@outlook.com>
2023-06-07 20:49:40 -0500

    added logout functionality at random place

--- src/components/TopNavigation/TopNavigation.tsx
+++ src/components/TopNavigation/TopNavigation.tsx
@@ -1,32 +1,48 @@
-import { Layout, Menu, MenuProps } from "antd";
-import { Header } from "antd/es/layout/layout";
+import React from "react";
+import { Layout, Button, Menu, MenuProps } from "antd";
+import { useNavigate } from "react-router-dom";
+import { auth } from "../../lib/firebase";
 import {
   LaptopOutlined,
   NotificationOutlined,
   UserOutlined,
 } from "@ant-design/icons";
 
-import React from "react";
+const { Header } = Layout;
 
-const TopNavigation = () => {
-  const { Header, Content, Sider } = Layout;
+const TopNavigation: React.FC = () => {
+  const navigate = useNavigate();
+
+  const handleLogout = async () => {
+    try {
+      await auth.signOut();
+      navigate("/");
+    } catch (error) {
+      console.error("Error occurred during logout:", error);
+    }
+  };
 
   const items1: MenuProps["items"] = ["1", "2", "3"].map((key) => ({
     key,
     label: `nav ${key}`,
   }));
+
   return (
-    <>
-      <Header style={{ display: "flex", alignItems: "center" }}>
-        <div className="demo-logo" />
-        <Menu
-          theme="dark"
-          mode="horizontal"
-          defaultSelectedKeys={["2"]}
-          items={items1}
-        />
-      </Header>
-    </>
+    <Header style={{ background: "#fff", padding: 0, display: "flex", alignItems: "center" }}>
+      <div className="demo-logo" />
+      <Menu
+        theme="dark"
+        mode="horizontal"
+        defaultSelectedKeys={["2"]}
+        items={items1}
+      />
+      <div style={{ flex: 1 }} />
+      <div style={{ paddingRight: "2rem" }}>
+        <Button type="primary" onClick={handleLogout}>
+          Logout
+        </Button>
+      </div>
+    </Header>
   );
 };
 
