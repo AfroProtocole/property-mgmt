daa381771873e7cf87426ab394a57df964e0d79b
kelvis24 <elviskimara@outlook.com>
2023-06-23 02:12:27 -0500

    added more tabs to the side bar

--- src/components/SideNavigation/SideNavigation.tsx
+++ src/components/SideNavigation/SideNavigation.tsx
@@ -1,15 +1,15 @@
-import { Layout, Menu, MenuProps, theme } from "antd";
-import Sider from "antd/es/layout/Sider";
+import React from "react";
+import { Layout, Menu, theme } from "antd";
+import { Link } from "react-router-dom";
 import {
-  LaptopOutlined,
-  NotificationOutlined,
-  UserOutlined,
   RightOutlined,
   DeploymentUnitOutlined,
   UserAddOutlined,
+  UsergroupAddOutlined,
+  BellOutlined,
+  CalendarOutlined,
+  ProfileOutlined,
 } from "@ant-design/icons";
-import React from "react";
-import { Link } from "react-router-dom";
 
 const SideNavigation = () => {
   const { Sider } = Layout;
@@ -18,39 +18,48 @@ const SideNavigation = () => {
   } = theme.useToken();
 
   const [isMenuCollapsed, setIsMenuCollpased] = React.useState(false);
+
   return (
-    <>
-      <Sider
-        width={200}
-        style={{ background: colorBgContainer, marginRight: "2rem" }}
-        collapsible={true}
-        collapsed={isMenuCollapsed}
-        reverseArrow={false}
-        trigger={null}
-      >
-        <Menu
-          mode="inline"
-          defaultSelectedKeys={["1"]}
-          defaultOpenKeys={["sub1"]}
-          style={{ height: "100%", borderRight: 0 }}
+    <Sider
+      width={200}
+      style={{ background: colorBgContainer, marginRight: "2rem" }}
+      collapsible={true}
+      collapsed={isMenuCollapsed}
+      reverseArrow={false}
+      trigger={null}
+    >
+      <Menu mode="inline" defaultSelectedKeys={["1"]} defaultOpenKeys={["sub1"]} style={{ height: "100%", borderRight: 0 }}>
+        <Menu.Item
+          icon={<RightOutlined />}
+          onClick={() => {
+            setIsMenuCollpased(!isMenuCollapsed);
+          }}
         >
-          <Menu.Item
-            icon={<RightOutlined />}
-            onClick={() => {
-              setIsMenuCollpased(!isMenuCollapsed);
-            }}
-          >
-            {isMenuCollapsed ? "Show" : "Hide"}
-          </Menu.Item>
-          <Menu.Item icon={<UserAddOutlined />}>
-            <Link to="/signup">Create User </Link>
-          </Menu.Item>
-          <Menu.Item icon={<DeploymentUnitOutlined />}>
-            <Link to="/organization/new">Create Organization</Link>
-          </Menu.Item>
-        </Menu>
-      </Sider>
-    </>
+          {isMenuCollapsed ? "Show" : "Hide"}
+        </Menu.Item>
+        <Menu.Item icon={<UserAddOutlined />}>
+          <Link to="/signup">Create User</Link>
+        </Menu.Item>
+        <Menu.Item icon={<DeploymentUnitOutlined />}>
+          <Link to="/organization/new">Create Organization</Link>
+        </Menu.Item>
+        <Menu.Item icon={<UsergroupAddOutlined />}>
+          <Link to="/tenants">Tenants</Link>
+        </Menu.Item>
+        <Menu.Item icon={<BellOutlined />}>
+          <Link to="/finance">Finance</Link>
+        </Menu.Item>
+        <Menu.Item icon={<CalendarOutlined />}>
+          <Link to="/service-request">Service Request</Link>
+        </Menu.Item>
+        <Menu.Item icon={<ProfileOutlined />}>
+          <Link to="/etat-des-lieux">Etat des Lieux</Link>
+        </Menu.Item>
+        <Menu.Item icon={<CalendarOutlined />}>
+          <Link to="/calendar">Calendar</Link>
+        </Menu.Item>
+      </Menu>
+    </Sider>
   );
 };
 
