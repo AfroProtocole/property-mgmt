e121629afe19d3fb1566540a73309469f0f6140c
kelvis24 <elviskimara@outlook.com>
2023-06-25 23:32:12 -0500

    added images to landing page

--- src/pages/LandingPage/LandingPage.tsx
+++ src/pages/LandingPage/LandingPage.tsx
@@ -9,17 +9,17 @@ const LandingPage = () => {
         <p className="landing-page-subtitle">Track, analyze, and manage your real estate assets with ease.</p>
         <div className="landing-page-features">
           <div className="landing-page-feature">
-            <img src="/path/to/feature1.png" alt="Feature 1" className="landing-page-feature-image" />
+            <img src="/asset-mngt.jpeg" alt="Feature 1" className="landing-page-feature-image" />
             <h3>Asset Tracking</h3>
             <p>Effortlessly track and manage your real estate assets in one central location.</p>
           </div>
           <div className="landing-page-feature">
-            <img src="/path/to/feature2.png" alt="Feature 2" className="landing-page-feature-image" />
+            <img src="/financial-analysis.jpeg" alt="Feature 2" className="landing-page-feature-image" />
             <h3>Financial Analysis</h3>
             <p>Analyze the financial performance of your assets with detailed reports and insights.</p>
           </div>
           <div className="landing-page-feature">
-            <img src="/path/to/feature3.png" alt="Feature 3" className="landing-page-feature-image" />
+            <img src="/track-expenses.webp" alt="Feature 3" className="landing-page-feature-image" />
             <h3>Expense Management</h3>
             <p>Keep track of your expenses, streamline processes, and optimize your real estate operations.</p>
           </div>
